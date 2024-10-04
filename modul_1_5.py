876b39d1e1449b2dfc746dfd7afe7bb7d8d0873f
Abovetous <nastena.kozarezova@gmail.com>
2024-10-04 18:32:15 +0300

    Add files via upload

---
+++ modul_1_5.py
@@ -0,0 +1,9 @@
+#immutable_var = 1,2,3,"hockey","football"
+#print(immutable_var)
+#immutable_var[0]=5
+#print(immutable_var) нельзя менять так как символ являеться картежом.
+mutable_list=([1,2,3],4,'sistem', False)
+mutable_list[0][1]=7
+print(mutable_list)
+
+
